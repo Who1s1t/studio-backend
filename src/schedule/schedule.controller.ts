be897b87f76d8a83eef3b28591c6d90f3ddc3df2
OVER <dedsek965@gmail.com>
2023-08-05 20:22:33 +0300

    update schedule

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe} from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
@@ -8,6 +8,7 @@ export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() createScheduleDto: CreateScheduleDto) {
     return this.scheduleService.create(createScheduleDto);
   }
--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ScheduleController } from './schedule.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {NewsEntity} from "../news/entities/news.entity";
 
 @Module({
   controllers: [ScheduleController],
-  providers: [ScheduleService]
+  providers: [ScheduleService],
+  imports: [TypeOrmModule.forFeature([NewsEntity])]
 })
 export class ScheduleModule {}
